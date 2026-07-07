bf97ed9180c9e49d6a381407a769ae9b6b3c4c8c
andree004i <andrea.kontozoglou@studio.unibo.it>
2026-07-07 18:32:02 +0200

    Add saved locations view model

---
+++ app/src/main/java/com/example/parkmatee/ui/savedlocations/SavedLocationViewModel.kt
@@ -0,0 +1,74 @@
+package com.example.parkmatee.ui.savedlocations
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.example.parkmatee.data.entity.SavedLocation
+import com.example.parkmatee.data.repository.SavedLocationRepository
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+class SavedLocationViewModel(
+    private val repository: SavedLocationRepository
+) : ViewModel() {
+
+    val locations: StateFlow<List<SavedLocation>> = repository.getAllLocations()
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), emptyList())
+
+    fun addLocation(
+        name: String,
+        latitude: Double,
+        longitude: Double
+    ) {
+        viewModelScope.launch {
+            repository.insert(
+                SavedLocation(
+                    name = name,
+                    latitude = latitude,
+                    longitude = longitude
+                )
+            )
+        }
+    }
+
+    fun updateLocation(
+        location: SavedLocation,
+        name: String,
+        latitude: Double,
+        longitude: Double
+    ) {
+        viewModelScope.launch {
+            repository.update(
+                location.copy(
+                    name = name,
+                    latitude = latitude,
+                    longitude = longitude
+                )
+            )
+        }
+    }
+
+    fun deleteLocation(location: SavedLocation) {
+        viewModelScope.launch {
+            repository.delete(location)
+        }
+    }
+
+    class Factory(
+        private val repository: SavedLocationRepository
+    ) : ViewModelProvider.Factory {
+
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(SavedLocationViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return SavedLocationViewModel(repository) as T
+            }
+
+            throw IllegalArgumentException(
+                "Unknown ViewModel class: ${modelClass.name}"
+            )
+        }
+    }
+}
